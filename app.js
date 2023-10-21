25b293572587456f360cc5506da4d8cfb4725394
unknown <kiantpetersen@gmail.com>
2023-10-21 12:42:37 +0200

    adding v-for and toggleFav

--- app.js
+++ app.js
@@ -4,13 +4,48 @@ const app = Vue.createApp({
     // template: '<h2>I am the template </h2>'
     data() {
         return {
+            url: 'https://www.kiantpetersen.com',
             showBooks: true,
-            title: 'Ready Player One',
-            author: 'Ernest Cline',
-            age: 51
+            // title: 'Ready Player One',
+            // author: 'Ernest Cline',
+            // age: 51,
+            books: [
+                {
+                    title: 'Ready Player One',
+                    author: 'Ernest Cline',
+                    age: 52,
+                    img: 'assets/image_1.jpg',
+                    isFav: true,
+                },
+                {
+                    title: 'Green Lights',
+                    author: 'Matthew McConaughy',
+                    age: 53,
+                    img: 'assets/image_3.jpg',
+                    isFav: false,
+
+
+                }
+
+                ,
+                {
+                    title: 'Chaus Rising',
+                    author: 'Timothy Zahn',
+                    age: 72,
+                    img: 'assets/image_2.jpg',
+                    isFav: true,
+
+
+                }
+            ],
+            x: 0,
+            y: 0,
         }
     },
     methods: {
+        toggleFav(book) {
+            book.isFav = !book.isFav
+        },
         changeTitle(title) {
 
             this.title = title
@@ -18,8 +53,22 @@ const app = Vue.createApp({
         },
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log('Event', e, e.type)
+            data ? console.log(data) : null;
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
 
+    }
+    ,
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
+        }
     }
 })
 
